44bdc91689b76122da5fbcea9261cb55d1f49a30
Cézar Bezerra <cezar30santos@gmail.com>
2025-03-03 23:22:04 -0300

    feat: makes the post card display the cover image

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import styles from "./Post.module.scss";
 import Image from "next/image";
 import memphis from "../../../public/memphis.jpg";
+import example from "../../../public/example.png";
 import Favorite from "../favorite/Favorite";
 import CommentIcon from "@/icons/CommentIcon";
 import LikeIcon from "@/icons/LikeIcon";
@@ -22,35 +23,42 @@ const Post = () => {
   };
   return (
     <div className={styles.post}>
-      <div className={styles.header}>
-        <div className={styles.userInfos}>
-          <div className={styles.imgContainer}>
-            <Image src={memphis} alt="author" />
-          </div>
-          <div className={styles.infos}>
-            <Link href={""}>
-              <h3>{post.CreatedBy}</h3>
-            </Link>
-            <p>{post.CreatedAt}</p>
-          </div>
+      {post.CoverImageUrl && (
+        <div className={styles.cover}>
+          <Image src={example} alt="cover" />
         </div>
-        <Favorite
-          isFavorite={isFavorite}
-          favorite={favorite}
-          unfavorite={unfavorite}
-        />
-      </div>
-      <div className={styles.content}>
-        <Link href={""}>
-          <h1>{post.Title}</h1>
-        </Link>
-
-        <div className={styles.stats}>
-          <div className={styles.stat}>
-            <LikeIcon /> {post.LikeCount} {likeText}
+      )}
+      <div className={styles.body}>
+        <div className={styles.header}>
+          <div className={styles.userInfos}>
+            <div className={styles.imgContainer}>
+              <Image src={memphis} alt="author" />
+            </div>
+            <div className={styles.infos}>
+              <Link href={""}>
+                <h3>{post.CreatedBy}</h3>
+              </Link>
+              <p>{post.CreatedAt}</p>
+            </div>
           </div>
-          <div className={styles.stat}>
-            <CommentIcon /> {post.CommentCount} {commentText}
+          <Favorite
+            isFavorite={isFavorite}
+            favorite={favorite}
+            unfavorite={unfavorite}
+          />
+        </div>
+        <div className={styles.content}>
+          <Link href={""}>
+            <h1>{post.Title}</h1>
+          </Link>
+
+          <div className={styles.stats}>
+            <div className={styles.stat}>
+              <LikeIcon /> {post.LikeCount} {likeText}
+            </div>
+            <div className={styles.stat}>
+              <CommentIcon /> {post.CommentCount} {commentText}
+            </div>
           </div>
         </div>
       </div>
