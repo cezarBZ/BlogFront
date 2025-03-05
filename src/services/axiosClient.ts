b14f27d53c52f94313b1cabd5d3bc4ef440bd108
Cézar Bezerra <cezar30santos@gmail.com>
2025-03-04 23:02:19 -0300

    feat: Start Axios httClient

---
+++ src/services/axiosClient.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { destroyCookie, parseCookies } from "nookies";
+
+const axiosClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosClient.interceptors.request.use(
+  (config) => {
+    const cookies = parseCookies();
+    const token = cookies.token;
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      destroyCookie(null, "token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosClient;
